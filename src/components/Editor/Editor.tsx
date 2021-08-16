b103ff27c42f36c86c74c10382c6000359475e40
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-08-16 15:53:37 +0300

    working editor

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,16 +1,17 @@
 import './Editor.scss'
 import { schema } from 'prosemirror-schema-basic'
-import { EditorState, TextSelection } from 'prosemirror-state'
+import { EditorState, Selection, TextSelection } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
 import { undo, redo, history } from 'prosemirror-history'
 import { keymap } from 'prosemirror-keymap'
-import { baseKeymap } from 'prosemirror-commands'
-import { useEffect, useRef, MutableRefObject } from 'react'
+import { baseKeymap, toggleMark } from 'prosemirror-commands'
+import { useState, useEffect, useRef, MutableRefObject } from 'react'
 import MenuBar from '../MenuBar'
 
 export default function Editor() {
   const viewHost = useRef() as MutableRefObject<HTMLDivElement>
   const viewRef = useRef() as MutableRefObject<EditorView>
+  const [selection, setSelection] = useState(null as null | Selection)
 
   useEffect(() => {
     const state = EditorState.create({
@@ -21,38 +22,35 @@ export default function Editor() {
         keymap(baseKeymap),
       ],
     })
-    viewRef.current = new EditorView(viewHost.current, { state })
+    let prevSelection: null | Selection<any> = null
+    viewRef.current = new EditorView(viewHost.current, {
+      state,
+      dispatchTransaction(transaction) {
+        const changed = !prevSelection?.eq(transaction.selection)
+        prevSelection = transaction.selection
+        if (changed) setSelection(prevSelection)
+
+        const newState = viewRef.current.state.apply(transaction)
+        viewRef.current.updateState(newState)
+      },
+    })
     const { current: view } = viewRef
     view.focus()
-    view.dispatch(view.state.tr.insertText('Hello World'))
-    view.dispatch(
-      view.state.tr.setSelection(TextSelection.atEnd(view.state.doc)),
-    )
     return () => view.destroy()
   }, [])
 
   const editorActions = {
-    setBold() {
+    setMark(mark: string) {
       const { current: view } = viewRef
       view.focus()
-      const { state } = view
-      const isBold = state.doc
-        .nodeAt(state.selection.$from.pos)
-        ?.marks.some((mark) => mark.type.name === 'strong')
-      view.dispatch(
-        state.tr[isBold ? 'removeMark' : 'addMark'](
-          state.selection.$from.pos,
-          state.selection.$to.pos,
-          schema.mark('strong'),
-        ),
-      )
+      toggleMark(schema.marks[mark])(view.state, view.dispatch)
     },
   }
 
   return (
     <>
-      <MenuBar editorActions={editorActions} />
-      <div className="editor" ref={viewHost}></div>
+      <MenuBar editorActions={editorActions} selection={selection} />
+      <div className="editor" ref={viewHost} />
     </>
   )
 }
--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -1,19 +1,47 @@
 import './MenuBar.scss'
 import { MouseEventHandler } from 'react'
+import { Selection } from 'prosemirror-state'
 
 interface MenuBarProps {
   editorActions: { [key: string]: Function }
+  selection: null | Selection<any>
 }
 
-export default function MenuBar({ editorActions }: MenuBarProps) {
+export default function MenuBar({ editorActions, selection }: MenuBarProps) {
+  //   useEffect(() => {
+  //     console.log(
+  //       selection === null
+  //         ? 'no selection'
+  //         : selection.empty
+  //         ? 'nothing selected'
+  //         : 'content selected',
+  //     )
+  //   }, [selection])
   const boldHandler: MouseEventHandler = (e) => {
-    editorActions.setBold()
+    editorActions.setMark('strong')
+    e.preventDefault()
+  }
+  const italicHandler: MouseEventHandler = (e) => {
+    editorActions.setMark('em')
     e.preventDefault()
   }
 
   return (
     <div className="menuBar">
-      <button onClick={boldHandler}>B</button>
+      <button
+        style={{ fontWeight: 'bold' }}
+        className="menuBtn"
+        onClick={boldHandler}
+      >
+        B
+      </button>
+      <button
+        style={{ fontStyle: 'italic' }}
+        className="menuBtn"
+        onClick={italicHandler}
+      >
+        I
+      </button>
     </div>
   )
 }
