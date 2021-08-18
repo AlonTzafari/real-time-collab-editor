d51133cb7ebabcd3b6ae9906633e94a4fbdbe17c
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-08-18 17:11:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import './App.scss'
-// import Editor from '../../components/Editor'
 import Header from './components/Header'
-import usersContext from './contexts/usersContext'
 import Main from './pages/Main'
-import { useState } from 'react'
 
 export default function App() {
-  const [users, setUsers] = useState(
-    new Map() as Map<
-      number,
-      {
-        [x: string]: any
-      }
-    >,
-  )
-
   return (
     <div className="App">
-      <usersContext.Provider value={{ users, setUsers }}>
-        <Header />
-        <Main />
-      </usersContext.Provider>
+      <Header />
+      <Main />
     </div>
   )
 }
