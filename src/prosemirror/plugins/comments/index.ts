4c737002010eb1e20d41c8ac649384d1a2841c95
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-09-06 16:49:09 +0300

    delete unused code

--- src/prosemirror/plugins/comments/index.ts
+++ src/prosemirror/plugins/comments/index.ts
@@ -1,7 +1,6 @@
 import commentsPlugin from './commentsPlugin'
 import commentsKey from './commentsKey'
 import commentNodeSpec from './commentNode'
-import updateNodes from './updateNodes'
 import addComment from './addComment'
 
-export { commentsPlugin, commentsKey, commentNodeSpec, updateNodes, addComment }
+export { commentsPlugin, commentsKey, commentNodeSpec, addComment }
